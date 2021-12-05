62396640b894edd0c8c445132961093665fade2f
manhei3 <wilsoncmh2006@gmail.com>
2021-12-05 07:24:30 +0000

    moodtracker

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,14 @@
     import Greeting from '$lib/Greeting.svelte'
     import Entry from '$lib/Entry.svelte'
     import EntryModal from '$lib/EntryModal.svelte'
+    import supabase from '$lib/db';
+
+async function signOut() {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) alert(error.message); // alert if error
+}
+
 </script>
 <Greeting />
 <!-- Entries -->
@@ -18,5 +26,9 @@
         
    	 
     </div>
+</section>
+<!-- Sign Out -->
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={signOut}>Logout</button>
 </section>
 <EntryModal />
